e68ec3ac437462217c4d0635dc2da17f584e0666
alex21012 <66181239+alex21012@users.noreply.github.com>
2023-09-25 01:49:22 +0000

    Part 1

--- BitManipulation/bit.c
+++ BitManipulation/bit.c
@@ -2,14 +2,28 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+int GetBit(int num, int pos) {
+    return (num >> pos) & 1;
+}
 
-int GetBit(int num, int pos);
-int SetBit(int num, int pos);
-int ClearBit(int num, int pos);
-int Reversbit(int num);
+int SetBit(int num, int pos) {
+    return num | (1 << pos);
+}
+
+int ClearBit(int num, int pos) {
+    return num & ~(1 << pos);
+}
+
+int ReverseBit(int num) {
+    int result = 0;
+    for (int i = 0; i < 32; i++) {
+        result = (result << 1) | (num & 1);
+        num >>= 1;
+    }
+    return result;
+}
 int CountBit(int num);
 int PowerofTwo(unsigned int num);
-
 
 int main(int argc, char* argv[]){
 	(void)argc;
